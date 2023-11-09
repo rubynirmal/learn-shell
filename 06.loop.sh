7d26925f1cf23dcf74996f893f39771e5948b17d
NirmalRuby <nnirmal144@gmail.com>
2023-11-09 13:23:12 +0530

    expense.conf project #comments #print(echo)

---
+++ 06.loop.sh
@@ -0,0 +1,17 @@
+# while & for Loop
+
+#choose while loop if you are using expression
+#choose for loop if you having inputs
+
+#Loop is going to execute same set of commands again and again in iterative way
+a=10
+while [ "$a" -gt 0]; do
+  echo hello world
+  a=$(($a-1))
+  done
+
+  a=100
+  while [ "$a" -gt 0]; do
+    echo Nirmal Ruby
+    a=$(($a-1))
+    done
